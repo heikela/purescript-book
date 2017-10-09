51b01f56853d0d51c71635a8fd17c5935232fd36
Mikko Heikelä <mikko.heikela@gmail.com>
2017-10-09 22:15:22 +0100

    Define circleArea function and declare main's type

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -1,10 +1,15 @@
 module Main where
 
-import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
-import Math (sqrt)
+import Prelude ((+), (*), Unit)
+import Control.Monad.Eff.Console (logShow, CONSOLE)
+import Control.Monad.Eff (Eff)
+import Math (sqrt, pi)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+circleArea :: Number -> Number
+circleArea r = pi * r * r
+
+main :: Eff (console :: CONSOLE) Unit
 main = logShow (diagonal 3.0 4.0)
