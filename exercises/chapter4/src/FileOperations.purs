7feeb78e442caedd2e3a0ed5716c8776dea0e83e
Mikko Heikelä <mikko.heikela@futurice.com>
2017-10-14 21:48:00 +0100

    cartesian products, pythagorean triplets and factorizations

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -3,9 +3,10 @@ module FileOperations where
 import Prelude
 
 import Data.Path (Path, ls)
-import Data.Array (concatMap, filter, (:), null)
+import Data.Array (concat, concatMap, filter, (:), (..), null)
 import Data.Array.Partial (head, tail)
 import Partial.Unsafe (unsafePartial)
+import Control.MonadZero (guard)
 
 allFiles :: Path -> Array Path
 allFiles root = root : concatMap allFiles (ls root)
@@ -40,3 +41,33 @@ squares = map \x -> x * x
 
 nonNegatives :: Array Int -> Array Int
 nonNegatives arr = (\x -> if x < 0 then false else true) <$?> arr
+
+factors :: Int -> Array (Array Int)
+factors n = do
+  i <- 1..n
+  j <- i..n
+  guard $ i * j == n
+  pure [i, j]
+
+cartesian :: forall a. Array a -> Array a -> Array (Array a)
+cartesian arr1 arr2 = do
+  i <- arr1
+  j <- arr2
+  pure [i, j]
+
+pythagoreanTriples :: Int -> Array (Array Int)
+pythagoreanTriples n = do
+  a <- 1..n
+  b <- a..n
+  c <- b..n
+  guard $ a * a + b * b == c * c
+  pure [a, b, c]
+
+factorizations :: Int -> Array (Array Int)
+factorizations 1 = [[1]]
+factorizations n = do
+  a <- 2..n
+  b <- 1..n
+  guard $ a * b == n
+  factorizationsOfB <- factorizations b
+  pure $ a : factorizationsOfB
